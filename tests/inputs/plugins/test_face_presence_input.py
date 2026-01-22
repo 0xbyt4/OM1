61de9a67cf4f79aabd1a23066792ce4c71eb2631
0xbyt4 <35742124+0xbyt4@users.noreply.github.com>
2026-01-22 13:23:04 +0300

    chore: remove redundant docstrings from test file

--- tests/inputs/plugins/test_face_presence_input.py
+++ tests/inputs/plugins/test_face_presence_input.py
@@ -1,13 +1,9 @@
-"""Tests for face_presence_input module."""
-
 import logging
 from queue import Full, Queue
 from unittest.mock import MagicMock, patch
 
 
 class TestFacePresenceMessageHandler:
-    """Tests for _handle_face_message method logging behavior."""
-
     def test_logs_warning_when_retry_put_fails(self, caplog):
         """
         Test that a warning is logged when queue retry put fails.
